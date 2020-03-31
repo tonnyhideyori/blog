4347ad850e007ca5d92a064fbe2fc22aa4e818f9
Edwin Ismail <typhoon_ed@yahoo.com>
2020-03-31 17:26:40 +0200

    root file update

--- index.js
+++ index.js
@@ -18,11 +18,16 @@ require('./routers/content.js')(app)
 require('./routers/upload')(app)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/bluid"));
+  app.use(express.static("client/build"));
   const path = require('path')
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname,"client","build","index.html"))
   })
+  /*const root = require("path").join(__dirname, "client", "build");
+  app.use(express.static(root));
+  app.get("*", (req, res) => {
+    res.sendFile("index.html", { root });
+  });*/
 }
 
 const PORT = process.env.PORT || 5600
